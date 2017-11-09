d676e85dc926fdc24fedeb68c939af8a1bb35bf3
Jeff Verkoeyen <featherless@google.com>
2017-11-09 14:00:46 -0500

    Add docs and tests.

--- src/private/CABasicAnimation+MotionAnimator.m
+++ src/private/CABasicAnimation+MotionAnimator.m
@@ -47,7 +47,6 @@ CABasicAnimation *MDMAnimationFromTiming(MDMMotionTiming timing, CGFloat timeSca
       spring.mass = timing.curve.data[MDMSpringMotionCurveDataIndexMass];
       spring.stiffness = timing.curve.data[MDMSpringMotionCurveDataIndexTension];
       spring.damping = timing.curve.data[MDMSpringMotionCurveDataIndexFriction];
-      spring.initialVelocity = timing.curve.data[MDMSpringMotionCurveDataIndexInitialVelocity];
 
       // This API is only available on iOS 9+
       if ([spring respondsToSelector:@selector(settlingDuration)]) {
@@ -62,23 +61,62 @@ CABasicAnimation *MDMAnimationFromTiming(MDMMotionTiming timing, CGFloat timeSca
   return animation;
 }
 
-void MDMMakeAnimationAdditive(CABasicAnimation *animation) {
+void MDMConfigureAnimation(CABasicAnimation *animation,
+                           BOOL wantsAdditive,
+                           MDMMotionTiming timing) {
+  if (!wantsAdditive && timing.curve.type != MDMMotionCurveTypeSpring) {
+    return; // Nothing to do here.
+  }
+
+  // We can't infer the from/to value types from the animation if the values are NSValue types, so
+  // we map known key paths to their data types here:
   static NSSet *sizeKeyPaths = nil;
   static NSSet *positionKeyPaths = nil;
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
     sizeKeyPaths = [NSSet setWithArray:@[@"bounds.size"]];
-
-    positionKeyPaths = [NSSet setWithArray:@[@"position",
-                                             @"anchorPoint"]];
+    positionKeyPaths = [NSSet setWithArray:@[@"position", @"anchorPoint"]];
   });
 
   if ([animation.toValue isKindOfClass:[NSNumber class]]) {
-    CGFloat currentValue = (CGFloat)[animation.fromValue doubleValue];
-    CGFloat delta = currentValue - (CGFloat)[animation.toValue doubleValue];
-    animation.fromValue = @(delta);
-    animation.toValue = @0;
-    animation.additive = true;
+    // Non-additive animations animate along a direct path between fromValue and toValue, regardless
+    // of the model layer. Additive animations, on the other hand, animate towards the layer's model
+    // value by applying this formula:
+    //
+    //     presentationLayer.value = modelLayer.value + additiveAnim1.value ... additiveAnimN.value
+    //
+    // This formula is what allows additive animations to give the appearance of conservation of
+    // momentum when multiple additive animations are added to the same key path.
+    //
+    // To transform a non-additive animation into an additive animation, use the following formula:
+    //
+    //     additiveAnimation.from = -(animation.to - animation.from)
+    //     additiveAnimation.to   = 0
+    //
+    // For example, if we're animating from 50 to 100, our additive animation's from value will
+    // equal -(100 - 50) = -50. Because the accumulator is animating to 0 and our model layer is
+    // set to the destination value, our animation will give the appearance of animating from 50 to
+    // 100:
+    //
+    //  | model value | accumulator | presentation value |
+    //  |-------------|-------------|--------------------|
+    //  |         100 |         -50 |                 50 |
+    //  |         100 |         -25 |                 75 |
+    //  |         100 |         -10 |                 90 |
+    //  |         100 |          -5 |                 95 |
+    //  |         100 |           0 |                100 |
+    //
+    //
+    CGFloat from = (CGFloat)[animation.fromValue doubleValue];
+    CGFloat to = (CGFloat)[animation.toValue doubleValue];
+    CGFloat displacement = to - from;
+    CGFloat additiveDisplacement = -displacement;
+
+    if (wantsAdditive) {
+      animation.fromValue = @(additiveDisplacement);
+      animation.toValue = @0;
+      animation.additive = true;
+    }
 
 #pragma clang diagnostic push
     // CASpringAnimation is a private API on iOS 8 - we're able to make use of it because we're
@@ -87,26 +125,59 @@ void MDMMakeAnimationAdditive(CABasicAnimation *animation) {
     if ([animation isKindOfClass:[CASpringAnimation class]]) {
       CASpringAnimation *springAnimation = (CASpringAnimation *)animation;
 #pragma clang diagnostic pop
-      // We expect initialVelocity to originally be in absolute coordinate space, so here we convert
-      // it to Core Animation's normalized coordinate space.
-      springAnimation.initialVelocity = springAnimation.initialVelocity / -delta;
+
+      CGFloat absoluteInitialVelocity = timing.curve.data[MDMSpringMotionCurveDataIndexInitialVelocity];
+
+      // Our timing's initialVelocity is in points per second, but Core Animation expects initial
+      // velocity to be in terms of displacement per second.
+      //
+      // From the UIView animateWithDuration header docs:
+      //
+      // "[initialVelocity is] a unit coordinate system, where 1 is defined as traveling the total
+      //  animation distance in a second. So if you're changing an object's position by 200pt in
+      //  this animation, and you want the animation to behave as if the object was moving at
+      //  100pt/s before the animation started, you'd pass 0.5. You'll typically want to pass 0 for
+      //  the velocity."
+      //
+      // It's also important to know that an initial velocity > 0 indicates movement towards the
+      // destination, while an initial velocity < 0 indicates movement away from the destination.
+      //
+      // With this in mind, consider Core Animation's initialVelocity as having two bits of
+      // information:
+      //
+      // - Its sign. Positive is towards the destination. Negative is away.
+      // - Its amplitude, where amplitude * displacement = absolute initial velocity
+      //
+      // For example: If our initialVelocity is +200/s, and our displacement is -100, then our
+      // initialVelocity is -2, with the (-) indicating that we're moving away from the destination
+      // and the 2 indicating we're moving twice the displacement over a second. Similarly, if our
+      // initialVelocity is -200/s, and our displacement is still -100 points, then our
+      // initialVelocity is 2; only the sign has changed.
+      //
+      // We want to know amplitude, so we do some basic arithmetic to turn:
+      //     amplitude * displacement = absolute initial velocity
+      // into:
+      //     amplitude = absolute initial velocity / displacement
+      //
+      // As for our sign, if absoluteInitialVelocity matches the direction of displacement, then our
+      // sign will be positive. Otherwise, our sign will be negative, as expected by Core Animation.
+
+      springAnimation.initialVelocity = absoluteInitialVelocity / displacement;
     }
 
   } else if ([sizeKeyPaths containsObject:animation.keyPath]) {
-    CGSize currentValue = [animation.fromValue CGSizeValue];
-    CGSize destinationValue = [animation.toValue CGSizeValue];
-    CGSize delta = CGSizeMake(currentValue.width - destinationValue.width,
-                              currentValue.height - destinationValue.height);
-    animation.fromValue = [NSValue valueWithCGSize:delta];
+    CGSize from = [animation.fromValue CGSizeValue];
+    CGSize to = [animation.toValue CGSizeValue];
+    CGSize additiveDisplacement = CGSizeMake(from.width - to.width, from.height - to.height);
+    animation.fromValue = [NSValue valueWithCGSize:additiveDisplacement];
     animation.toValue = [NSValue valueWithCGSize:CGSizeZero];
     animation.additive = true;
 
   } else if ([positionKeyPaths containsObject:animation.keyPath]) {
-    CGPoint currentValue = [animation.fromValue CGPointValue];
-    CGPoint destinationValue = [animation.toValue CGPointValue];
-    CGPoint delta = CGPointMake(currentValue.x - destinationValue.x,
-                                currentValue.y - destinationValue.y);
-    animation.fromValue = [NSValue valueWithCGPoint:delta];
+    CGPoint from = [animation.fromValue CGPointValue];
+    CGPoint to = [animation.toValue CGPointValue];
+    CGPoint additiveDisplacement = CGPointMake(from.x - to.x, from.y - to.y);
+    animation.fromValue = [NSValue valueWithCGPoint:additiveDisplacement];
     animation.toValue = [NSValue valueWithCGPoint:CGPointZero];
     animation.additive = true;
 
@@ -120,12 +191,13 @@ void MDMMakeAnimationAdditive(CABasicAnimation *animation) {
       // Core Animation's velocity system is single dimensional, so we pick the dominant direction
       // of movement and normalize accordingly.
       CGFloat biggestDelta;
-      if (fabs(delta.x) > fabs(delta.y)) {
-        biggestDelta = delta.x;
+      if (fabs(additiveDisplacement.x) > fabs(additiveDisplacement.y)) {
+        biggestDelta = additiveDisplacement.x;
       } else {
-        biggestDelta = delta.y;
+        biggestDelta = additiveDisplacement.y;
       }
-      springAnimation.initialVelocity = springAnimation.initialVelocity / -biggestDelta;
+      CGFloat displacement = -biggestDelta;
+      springAnimation.initialVelocity = springAnimation.initialVelocity / displacement;
     }
   }
 }
---
+++ tests/unit/InitialVelocityTests.swift
@@ -0,0 +1,114 @@
+/*
+ Copyright 2017-present The Material Motion Authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import XCTest
+import MotionAnimator
+
+@available(iOS 9.0, *)
+class InitialVelocityTests: XCTestCase {
+
+  var animator: MotionAnimator!
+  var addedAnimations: [CAAnimation]!
+  override func setUp() {
+    super.setUp()
+
+    animator = MotionAnimator()
+    addedAnimations = []
+    animator.addCoreAnimationTracer { (_, animation) in
+      self.addedAnimations.append(animation)
+    }
+  }
+
+  override func tearDown() {
+    animator = nil
+    addedAnimations = nil
+
+    super.tearDown()
+  }
+
+  func testVelocityAmplitudeMatchesDisplacementWithPositiveDisplacement() {
+    let timing = MotionTiming(delay: 0,
+                              duration: 0.7,
+                              curve: .init(type: .spring, data: (1, 1, 1, 50)),
+                              repetition: .init(type: .none, amount: 0, autoreverses: false))
+    animator.animate(with: timing, to: CALayer(), withValues: [0, 100], keyPath: .opacity)
+
+    XCTAssertEqual(addedAnimations.count, 1)
+    let animation = addedAnimations.first as! CASpringAnimation
+    XCTAssertEqual(animation.initialVelocity, 0.5)
+  }
+
+  func testVelocityAmplitudeMatchesDisplacementWithNegativeDisplacement() {
+    let timing = MotionTiming(delay: 0,
+                              duration: 0.7,
+                              curve: .init(type: .spring, data: (1, 1, 1, -50)),
+                              repetition: .init(type: .none, amount: 0, autoreverses: false))
+    animator.animate(with: timing, to: CALayer(), withValues: [100, 0], keyPath: .opacity)
+
+    XCTAssertEqual(addedAnimations.count, 1)
+    let animation = addedAnimations.first as! CASpringAnimation
+    XCTAssertEqual(animation.initialVelocity, 0.5)
+  }
+
+  func testVelocityTowardsDestinationIsPositiveWithPositiveDisplacement() {
+    let timing = MotionTiming(delay: 0,
+                          duration: 0.7,
+                          curve: .init(type: .spring, data: (1, 1, 1, 100)),
+                          repetition: .init(type: .none, amount: 0, autoreverses: false))
+    animator.animate(with: timing, to: CALayer(), withValues: [0, 100], keyPath: .opacity)
+
+    XCTAssertEqual(addedAnimations.count, 1)
+    let animation = addedAnimations.first as! CASpringAnimation
+    XCTAssertGreaterThan(animation.initialVelocity, 0)
+  }
+
+  func testVelocityAwayFromDestinationIsNegativeWithPositiveDisplacement() {
+    let timing = MotionTiming(delay: 0,
+                              duration: 0.7,
+                              curve: .init(type: .spring, data: (1, 1, 1, -100)),
+                              repetition: .init(type: .none, amount: 0, autoreverses: false))
+    animator.animate(with: timing, to: CALayer(), withValues: [0, 100], keyPath: .opacity)
+
+    XCTAssertEqual(addedAnimations.count, 1)
+    let animation = addedAnimations.first as! CASpringAnimation
+    XCTAssertLessThan(animation.initialVelocity, 0)
+  }
+
+  func testVelocityTowardsDestinationIsPositiveWithNegativeDisplacement() {
+    let timing = MotionTiming(delay: 0,
+                              duration: 0.7,
+                              curve: .init(type: .spring, data: (1, 1, 1, -100)),
+                              repetition: .init(type: .none, amount: 0, autoreverses: false))
+    animator.animate(with: timing, to: CALayer(), withValues: [100, 0], keyPath: .opacity)
+
+    XCTAssertEqual(addedAnimations.count, 1)
+    let animation = addedAnimations.first as! CASpringAnimation
+    XCTAssertGreaterThan(animation.initialVelocity, 0)
+  }
+
+  func testVelocityAwayFromDestinationIsNegativeWithNegativeDisplacement() {
+    let timing = MotionTiming(delay: 0,
+                              duration: 0.7,
+                              curve: .init(type: .spring, data: (1, 1, 1, 100)),
+                              repetition: .init(type: .none, amount: 0, autoreverses: false))
+    animator.animate(with: timing, to: CALayer(), withValues: [100, 0], keyPath: .opacity)
+
+    XCTAssertEqual(addedAnimations.count, 1)
+    let animation = addedAnimations.first as! CASpringAnimation
+    XCTAssertLessThan(animation.initialVelocity, 0)
+  }
+}
+
