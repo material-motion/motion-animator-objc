9c97c175adb79037fbf93a7c218c4ba473b5f09a
Jeff Verkoeyen <featherless@google.com>
2017-11-15 14:26:14 -0500

    Docs.

--- src/private/MDMAnimationRegistrar.h
+++ src/private/MDMAnimationRegistrar.h
@@ -17,15 +17,22 @@
 #import <Foundation/Foundation.h>
 #import <QuartzCore/QuartzCore.h>
 
-// Tracks animations that have been added to a layer.
+// Tracks and manipulates animations that have been added to a layer.
 @interface MDMAnimationRegistrar : NSObject
 
-- (void)addAnimation:(CABasicAnimation *)animation
-             toLayer:(CALayer *)layer
-              forKey:(NSString *)key
-          completion:(void(^)(void))completion;
+// Invokes the layer's addAnimation:forKey: method with the provided animation and key and tracks
+// its association. Upon completion of the animation, the provided optional completion block will be
+// executed.
+- (void)addAnimation:(nonnull CABasicAnimation *)animation
+             toLayer:(nonnull CALayer *)layer
+              forKey:(nonnull NSString *)key
+          completion:(void(^ __nullable)(void))completion;
 
+// For every active animation, reads the associated layer's presentation layer key path and writes
+// it to the layer.
 - (void)commitCurrentAnimationValuesToAllLayers;
+
+// Removes all active animations from their associated layer.
 - (void)removeAllAnimations;
 
 @end
